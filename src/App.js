a1b94f85d17e9dc5f1a74b2a71c0c14f32ff89f1
EstefaRios <estefaniarios031101@gmail.com>
2025-05-21 11:18:33 -0500

    Segundo ajuste

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
